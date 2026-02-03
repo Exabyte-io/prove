2203ef0b42a10b9657c80693f5e8b4c7a272608a
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-03 15:02:16 -0800

    update: run

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vite";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: "/prove/",
-    plugins: [react(), nodePolyfills()],
+    base: "./",
+    plugins: [
+        react({
+            jsxImportSource: "@emotion/react",
+            babel: {
+                plugins: ["@emotion/babel-plugin"],
+            },
+        }),
+        nodePolyfills({
+            include: ["path", "process"],
+            globals: {
+                process: true,
+            },
+        }),
+    ],
+    resolve: {
+        dedupe: [
+            "react",
+            "react-dom",
+            "@emotion/react",
+            "@emotion/styled",
+            "@mui/material",
+            "@mui/system",
+            "@mui/styled-engine",
+        ],
+    },
     define: {
         __dirname: JSON.stringify(__dirname),
     },
